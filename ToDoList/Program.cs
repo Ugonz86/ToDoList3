6fc30d92c538e8016a37082494d90780f730041a
Na Hyung Choi and Uriel Gonzalez <uriel@>
2019-07-23 11:05:08 -0700

    add console UI

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,10 +1,51 @@
 using System;
+using System.Collections.Generic;
 using ToDoList.Models;
 
 class Program
 {
+    private static string userInput = "";
+
+    private static void PromptUser()
+    {
+        Console.Write("Would you like to add an item to your list or view your list? (Add/View): ");
+        userInput = Console.ReadLine();
+
+        if (userInput == "Add" || userInput == "add" || userInput == "a")
+        {
+            Console.Write("Your new to-do item: ");
+            userInput = Console.ReadLine();
+
+            Item newItem = new Item(userInput);
+            Console.WriteLine($"\"{userInput}\" has been added to your list.");
+            PromptUser();
+        }
+        else if (userInput == "View" || userInput == "view" || userInput == "v")
+        {
+            List<Item> itemList = Item.GetAll();
+            if (itemList.Count < 1)
+            {
+                Console.WriteLine("There are no items on your list.");
+            }
+            else
+            {
+                for (int i = 0; i < itemList.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {itemList[i].Description}");
+                }
+            }
+            PromptUser();
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid input.");
+            PromptUser();
+        }
+    }
+
     private static void Main()
     {
-
+        Console.WriteLine("Welcome to the To Do List!");
+        PromptUser();
     }
 }
