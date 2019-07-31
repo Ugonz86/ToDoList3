976100feef07a74f59a6ca0c81bc98e18f79c23c
Ugonz86 <ugonzalez86@gmail.com>
2019-07-31 08:08:01 -0700

    Adding new files

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -7,31 +7,29 @@ namespace ToDoList.Controllers
   public class ItemsController : Controller
   {
 
-    [HttpGet("/items")]
-    public ActionResult Index()
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
+       Category category = Category.Find(categoryId);
+       return View(category);
     }
 
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-      return View();
-    }
-
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
     }
 
     [HttpPost("/items/delete")]
     public ActionResult DeleteAll()
     {
-        Item.ClearAll();
-        return View();
+      Item.ClearAll();
+      return View();
     }
 
   }
